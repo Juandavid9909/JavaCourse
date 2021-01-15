c8c8ae3f372b0d2afb9bb93fd0da7f9d9aac347f
Juan David <juandavid918@gmail.com>
2021-01-15 14:44:30 -0500

    Laboratorio final fundamentos finalizado

---
+++ Fundamentos/Ventas/src/mx/com/gm/ventas/Producto.java
@@ -0,0 +1,48 @@
+package mx.com.gm.ventas;
+
+public class Producto {
+    private final int idProducto;
+    private String nombre;
+    private double precio;
+    private static int contadorProductos;
+    
+    private Producto() {
+        this.idProducto = ++Producto.contadorProductos;
+    }
+    
+    public Producto(String nombre, double precio) {
+        this();
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    public int getIdProducto() {
+        return this.idProducto;
+    }
+    
+    public String getNombre() {
+        return this.nombre;
+    }
+    
+    public double getPrecio() {
+        return this.precio;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Producto{idProducto=").append(idProducto);
+        sb.append(", nombre=").append(nombre);
+        sb.append(", precio=").append(precio);
+        sb.append('}');
+        return sb.toString();
+    }
+}
