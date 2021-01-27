0683053baaa90621f013fd5ebe6bc080986735c8
Juan David <juandavid918@gmail.com>
2021-01-27 14:06:58 -0500

    Sección Introducción JDBC finalizada

--- Programacion/CatalogoPeliculas/src/mx/com/gm/peliculas/servicio/CatalogoPeliculasImpl.java
+++ Programacion/CatalogoPeliculas/src/mx/com/gm/peliculas/servicio/CatalogoPeliculasImpl.java
@@ -1,10 +1,9 @@
 package mx.com.gm.peliculas.servicio;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import mx.com.gm.peliculas.datos.*;
 import mx.com.gm.peliculas.domain.Pelicula;
 import mx.com.gm.peliculas.excepciones.AccesoDatosEx;
+import mx.com.gm.peliculas.excepciones.LecturaDatosEx;
 
 public class CatalogoPeliculasImpl implements ICatalogoPeliculas {
     
@@ -30,13 +29,45 @@ public class CatalogoPeliculasImpl implements ICatalogoPeliculas {
 
     @Override
     public void listarPeliculas() {
+        try {
+            var peliculas = this.datos.listar(NOMBRE_RECURSO);
+            
+            for(var pelicula : peliculas) {
+                System.out.println("pelicula = " + pelicula);
+            }
+        }
+        catch (AccesoDatosEx ex) {
+            System.out.println("Error de acceso datos");
+            ex.printStackTrace(System.out);
+        }
     }
 
     @Override
     public void buscarPelicula(String buscar) {
+        String resultado = null;
+        try {
+            resultado = this.datos.buscar(NOMBRE_RECURSO, buscar);
+        }
+        catch (AccesoDatosEx ex) {
+            System.out.println("Error de acceso datos");
+            ex.printStackTrace();
+        }
+        System.out.println("resultado = " + resultado);
     }
 
     @Override
     public void iniciarCatalogoPeliculas() {
+        try {
+            if(this.datos.existe(NOMBRE_RECURSO)) {
+                datos.borrar(NOMBRE_RECURSO);
+                datos.crear(NOMBRE_RECURSO);
+            }
+            else {
+                datos.crear(NOMBRE_RECURSO);
+            }
+        } catch (AccesoDatosEx ex) {
+            System.out.println("Error al iniciar catalogo de peliculas");
+            ex.printStackTrace();
+        }
     }
 }
